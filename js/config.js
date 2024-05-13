233ac41c5dd46caa827e1d3aa89c9e247f027874
Kevin Cochran <40193276+kevincloud@users.noreply.github.com>
2024-05-13 17:50:16 -0400

    initial commit

---
+++ js/config.js
@@ -0,0 +1,14 @@
+
+const config = {
+    toast: "top",       // Set to "top" or "bottom" to change the position of the toast message.
+
+    // Start of product configuration
+    products: [
+        {
+            name: "MyCalculator",
+            publicKey: "MOBILE_APP_PUBLIC_KEY",
+            device: "pixel7"
+        }
+    ],
+    // End of product configuration.
+};
